7749b4a21bcf2f04fb7dcef92fcea887a68ad27c
seppelin <seppelin@gmx.de>
2023-12-22 17:28:12 +0100

    add menu.c

--- src/main.c
+++ src/main.c
@@ -1,18 +1,9 @@
 #include "raylib.h"
+#include "menu.h"
 #include "ui.h"
-#include <stdio.h>
 
 int main(void) {
 	InitWindow(1600, 900, "septic");
-	Button b = buttonInitText(10, 10, "hellox", 50, DARKPURPLE);
-	int quit = 0;
-	printf("yay!");
-	while(!quit && !WindowShouldClose()) {
-		BeginDrawing();
-		ClearBackground(GRAY);
-		quit = buttonTick(&b);
-		EndDrawing();
-	}
-	buttonDeinit(&b);
+	Scene scene = Menu;
 	return 0;
 }
